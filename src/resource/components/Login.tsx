85afb4bd05586fbcbf7ca00e8cea01da66f444f8
CanNguyen284 <nguyen.daocan@hcmut.edu.vn>
2024-11-12 18:46:47 +0700

    update FE

--- src/resource/components/Login.tsx
+++ src/resource/components/Login.tsx
@@ -1,34 +1,81 @@
-import Image from 'next/image';
-import '@/resource/styles/Login.css'; 
-import user_icon from '@/resource/images/person.png';
-import email_icon from '@/resource/images/email.png';
-import password_icon from '@/resource/images/password.png';
-import Link from 'next/link';
-const Login = () => { 
-    
-    return (
-        <div className="container">
-            <div className="header">
-                <div className="text">Login</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <Image src={ email_icon} alt="" />
-                    <input type="email" placeholder="Email"/>
-                </div>
-                <div className="input">
-                    <Image src={ password_icon} alt="" />
-                    <input type="password" placeholder="Password"/>
-                </div>
-            </div>
-            <div className="forgotPass">Lost Password ? <span>Click Here</span></div>
-            
-            <div className="submit-container">
-                
-                <div className="submit" >Login</div>
-            </div>
+"use client";
+import Image from "next/image";
+import "@/resource/styles/Login.css";
+import user_icon from "@/resource/images/person.png";
+import email_icon from "@/resource/images/email.png";
+import password_icon from "@/resource/images/password.png";
+import Link from "next/link";
+import { useState } from "react";
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  function LoginSubmit() {
+    const url = "https://ltwbe.hcmutssps.id.vn/auth/login";
+    const data = {
+      username: userName,
+      password: password,
+    };
+
+    fetch(url, {
+      method: "PUT", // Use the PUT method
+      headers: {
+        "Content-Type": "application/json", // Set the content type to JSON
+      },
+      body: JSON.stringify(data), // Convert the data object to a JSON string
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json(); // Parse the JSON from the response
+      })
+      .then((data) => {
+        console.log("Success:", data); // Handle the data from the response
+        const token = data.message;
+        const expiryDate = new Date();
+        expiryDate.setDate(expiryDate.getDate() + 1); // Set expiry date to 1 day from now
+        document.cookie = `token=${token}; expires=${expiryDate.toUTCString()}; path=/`;
+      })
+      .catch((error) => {
+        console.error("Error:", error); // Handle any errors
+      });
+  }
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Login</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <Image src={user_icon} alt="" />
+          <input
+            type="text"
+            placeholder="User Name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </div>
-    )
-}
+        <div className="input">
+          <Image src={password_icon} alt="" />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="forgotPass">
+        Do Not Have Account ? <span>Click Here</span>
+      </div>
+
+      <div className="submit-container">
+        <button className="submit" onClick={LoginSubmit}>
+          Login{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Login;
--- src/resource/components/Register.tsx
+++ src/resource/components/Register.tsx
@@ -1,38 +1,67 @@
+'use client'
 import Image from 'next/image';
 import '@/resource/styles/Login.css'; 
 import user_icon from '@/resource/images/person.png';
 import email_icon from '@/resource/images/email.png';
 import password_icon from '@/resource/images/password.png';
 import Link from 'next/link';
-const Login = () => { 
-    
-    return (
-        <div className="container">
-            <div className="header">
-                <div className="text">Sign Up</div>
-                <div className="underline"></div>
-            </div>
-            
-            <div className="inputs">
-                <div className="input">
-                    <Image src={ user_icon} alt="" />
-                    <input type="text" placeholder="Username"/>
-                </div>
-                <div className="input">
-                    <Image src={ email_icon} alt="" />
-                    <input type="email" placeholder="Email"/>
-                </div>
-                <div className="input">
-                    <Image src={ password_icon} alt="" />
-                    <input type="password" placeholder="Password"/>
-                </div>
-            </div>
-            
-            <div className="submit-container">
-                
-                <div className="submit" >Sign Up</div>
-            </div>
-        </div>
-    )
+function LoginSubmit() {
+    const url = 'https://ltwbe.hcmutssps.id.vn/auth/register';
+    const data = {
+        username: 'lisajones12',
+        email: 'lisajones12@gmail.com',
+        password: 'lisa1234'
+    };
+
+    fetch(url, {
+        method: 'PUT', // Use the PUT method
+        headers: {
+            'Content-Type': 'application/json' // Set the content type to JSON
+        },
+        body: JSON.stringify(data) // Convert the data object to a JSON string
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json(); // Parse the JSON from the response
+        })
+        .then(data => {
+            console.log('Success:', data); // Handle the data from the response
+        })
+        .catch(error => {
+            console.error('Error:', error); // Handle any errors
+        });
 }
-export default Login;
+    const Login = () => {
+    
+        return (
+            <div className="container">
+                <div className="header">
+                    <div className="text">Sign Up</div>
+                    <div className="underline"></div>
+                </div>
+            
+                <div className="inputs">
+                    <div className="input">
+                        <Image src={user_icon} alt="" />
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    <div className="input">
+                        <Image src={email_icon} alt="" />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="input">
+                        <Image src={password_icon} alt="" />
+                        <input type="password" placeholder="Password" />
+                    </div>
+                </div>
+            
+                <div className="submit-container">
+                
+                    <button className="submit" onClick={LoginSubmit} > Sign Up</button>
+                </div>
+            </div>
+        )
+    }
+    export default Login;
